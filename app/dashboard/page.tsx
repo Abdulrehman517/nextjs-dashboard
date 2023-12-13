1fb4b8b4593c089abe1314ed32610623df0312dd
Abdulrehman517 <abdulrehman517uetian@gmail.com>
2023-12-13 09:06:43 +0500

    added the skeleton and steaming for LatestInvoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,12 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {totalPaidInvoices,totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
 
   return (
@@ -28,8 +27,10 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
       <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
-        </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+      </Suspense>
+      <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices />
+      </Suspense>
       </div>
     </main>
   );
